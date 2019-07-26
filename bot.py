d76d30c1a269ca26abbe24bf8fb26fe8a6e0fbf2
howardgood88 <howardgood88@gmail.com>
2019-07-26 18:08:28 +0800

    更新helper換行

--- bot.py
+++ bot.py
@@ -14,13 +14,13 @@ voice_channel_id = 560861172648378393 #ours
 guild_id = 560861172648378389
 
 description = '''
-online bot用法：\n
-onilne bot的前導符號為"!"，任何指令前面都須加上才能執行。\n\n
+online bot用法：
+onilne bot的前導符號為"!"，任何指令前面都須加上才能執行。\n
 
-online bot的功能：\n
-紀錄成員上線時間。只要成員上線後進入任意語音頻道，\n
-online bot會偵測到並回應"I see you, xxx"，這時上線時間已開始累計，\n
-並於下線時(離開此群組也算)結算時數\n\n
+online bot的功能：
+紀錄成員上線時間。只要成員上線後進入任意語音頻道，
+online bot會偵測到並回應"I see you, xxx"，這時上線時間已開始累計，
+並於下線時(離開此群組也算)結算時數\n
 
 online bot提供之指令列表：\n
 '''
